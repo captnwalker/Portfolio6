ac36cf2858e1d6d8e20c4b3643b66733c2c9f2d3
Steve Walker <captnwalker@gmail.com>
2018-04-11 00:52:33 -0400

    sideNav styled

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   // Init sideNav
-  $('.button-collapse').sideNav();
+  $('.button-collapse').sideNav({
+    menuWidth: 200, // Default is 300
+    edge: 'left', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true // Choose whether you can drag to open on touch screens
+  });
 
   // Init modal
   $('.modal').modal();
